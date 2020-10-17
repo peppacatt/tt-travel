c78916a1b2510ecef15047b8a731379f76848176
peppacatt <peppacatt@163.com>
2020-10-18 00:18:41 +0800

    init

---
+++ src/main/java/com/baby/TtTravelApplication.java
@@ -0,0 +1,13 @@
+package com.baby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtTravelApplication.class, args);
+    }
+
+}
